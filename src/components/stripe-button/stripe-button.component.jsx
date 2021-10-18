f2131abaa2af4a9314c5f7a53bd058489da376bf
Harshit <harshitsinghchouhan2018@gmail.com>
2021-10-18 22:00:22 +0530

    stripe integrated

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51JlyJYSDken0oy26Mrj4nae6ypUiaCtbEwImOQqCM11uuQYbe4IiXHOPDfP2Te03QeG0KMn8YwBERYeTJN8EvWia00ezIK87WT';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeButton;
