e90c978b7b8e32b604b4624cba447b8e974b14f4
Harshit Chouhan <harshit.chouhan@asmadiya.com>
2023-05-31 07:51:35 +0530

    Sign In and Sign up funcationality done

---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,15 @@
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import SignupForm from "../../components/sign-up-form/sign-up-form.component";
+
+import "./authentication.styles.scss";
+
+const Authentication = () => {
+  return (
+    <div className="authentication-container">
+      <SignInForm />
+      <SignupForm />
+    </div>
+  );
+};
+
+export default Authentication;
