f487de1542329e4aca8a729ea53927106c359219
Harshit Chouhan <harshit.chouhan@asmadiya.com>
2023-06-05 09:11:25 +0530

    checkout page completed

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,20 @@
-import { useContext } from "react";
+import './cart-dropdown.styles.scss';
 
-import { CartContext } from "../../contexts/cart.context";
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import Button from "../button/button.component";
-import CartItem from "../cart-item/cart-item.component";
-
-import "./cart-dropdown.styles.scss";
+import { CartContext } from '../../contexts/cart.context';
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
 
   return (
     <div className="cart-dropdown-container">
@@ -21,7 +27,7 @@ const CartDropdown = () => {
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
